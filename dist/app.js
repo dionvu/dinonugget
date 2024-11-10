e8fd08385aae57f17bf07a78e6405d4f3bf355e8
Dion Vu <dionvu11@gmail.com>
2024-11-10 13:40:58 -0800

    init

---
+++ dist/app.js
@@ -0,0 +1,2 @@
+function name(a) { }
+export {};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html", "./src/style.css", "./src/app.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
