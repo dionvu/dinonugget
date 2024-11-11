638a64ee447079a1a3afd6280e91587224be2da4
Dion Vu <dionvu11@gmail.com>
2024-11-10 16:04:12 -0800

    finally figured out how to get stuff working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/index.html", "./src/style.css", "./src/app.ts"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = ["./src/**/*.{html,js}"];
+
+export const mode = "jit";
+
+export const theme = {
+  extend: {},
 };
+
+export const plugins = [];
